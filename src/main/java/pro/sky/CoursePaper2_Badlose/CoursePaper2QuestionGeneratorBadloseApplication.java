f27467e59e7310de15646466783bff58ccf64f69
Badlose <git@github.com:Badlose/helloworld.git>
2024-10-31 13:31:41 +0300

    Initial commit

---
+++ src/main/java/pro/sky/CoursePaper2_Badlose/CoursePaper2QuestionGeneratorBadloseApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.CoursePaper2_Badlose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePaper2QuestionGeneratorBadloseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePaper2QuestionGeneratorBadloseApplication.class, args);
+	}
+
+}
